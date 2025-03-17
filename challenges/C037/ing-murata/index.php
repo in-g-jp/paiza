6861fdedf65cb967d53f2edc22eacc6d278ba874
ing-murata <murata@in-g.jp>
2025-03-17 12:24:55 +0900

    説明の追加

--- challenges/C037/ing-murata/index.php
+++ challenges/C037/ing-murata/index.php
@@ -1,10 +1,14 @@
 <?php
 $inputs = explode(' ', fgets(STDIN));
 
+// /で分割
 list($month, $day) = explode('/', $inputs[0]);
+// :で分割
 list($hour, $minutes) = explode(':', $inputs[1]);
 
+//24で割った時の商をdayにたす
 $day = intval($day) + intdiv(intval($hour), 24);
+//24で割り切った時のあまりをhourにたす
 $hour = intval($hour) % 24;
 
 if ($hour < 10 && $day < 10) {
