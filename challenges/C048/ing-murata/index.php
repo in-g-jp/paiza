7b366287d18de50a4d8abbc5c859471edbd87291
ing-murata <murata@in-g.jp>
2025-03-21 12:11:01 +0900

    Update index.php

--- challenges/C048/ing-murata/index.php
+++ challenges/C048/ing-murata/index.php
@@ -1,6 +1,9 @@
 <?php
+//タダになるまでの値段を求める
 $input = explode(' ', fgets(STDIN));
+//価格
 $price = intval($input[0]);
+//割引率
 $discount  = intval($input[1]);
 $percentage = 1 - $n / 100;
 
@@ -11,6 +14,7 @@ while (true) {
     if ($price == 0) {
         break;
     }
+    //０になるまで値段を足し続ける
     $sum += $price;
 }
 echo $sum;
