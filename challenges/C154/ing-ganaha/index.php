d4b6c74b8b07a1c5dfef5b213f8a086d9b30741b
ing-ganaha <ganaha@in-g.jp>
2025-11-07 18:21:31 +0900

    phpファイル記入

--- challenges/C154/ing-ganaha/index.php
+++ challenges/C154/ing-ganaha/index.php
@@ -1,2 +1,14 @@
 <?php
-// ここに回答を実装してください
+list($itemCount,$border) = explode(' ',fgets(STDIN));
+    $itemValue = explode(' ',fgets(STDIN));
+    $maxIndex = array_search(max($itemValue),$itemValue);
+    if($itemValue[$maxIndex]>$border){
+        $itemValue[$maxIndex]=floor($itemValue[$maxIndex]/2);
+    }
+    
+    $totalValue=0;
+    for($i=0;$i<$itemCount;$i++){
+        $totalValue+=$itemValue[$i];
+    }
+    echo $totalValue;
+?>
