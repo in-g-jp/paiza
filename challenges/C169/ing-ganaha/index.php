664f62536c53c94188f417209f9f7395d621c377
ing-ganaha <ganaha@in-g.jp>
2025-11-07 18:25:33 +0900

    phpファイル記入

--- challenges/C169/ing-ganaha/index.php
+++ challenges/C169/ing-ganaha/index.php
@@ -1,2 +1,11 @@
 <?php
-// ここに回答を実装してください
+$myPoint = fgets(STDIN);
+    $depo=fgets(STDIN);
+    $totalPoint=0;
+    $week=7;
+    for($i=0;$i<$week;$i++){
+        $totalPoint+=$myPoint;
+        $myPoint=$myPoint*$depo/100;
+    }
+    echo $totalPoint;
+?>
