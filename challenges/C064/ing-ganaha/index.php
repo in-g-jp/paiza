b8ac0bcd04dc9cd8fa83886fd8990c7343e62627
ing-ganaha <ganaha@in-g.jp>
2025-11-09 22:05:24 +0900

    phpファイル記入

--- challenges/C064/ing-ganaha/index.php
+++ challenges/C064/ing-ganaha/index.php
@@ -1,2 +1,17 @@
 <?php
-// ここに回答を実装してください
+    list($M, $N) = array_map('intval', explode(" ", trim(fgets(STDIN))));
+    
+    $meel_cost = [];
+    for ($i = 0; $i < $M; $i++) {
+        $meel_cost[] = fgets(STDIN);
+    }
+
+    for ($i = 0; $i < $N; $i++) {
+        $amounts = explode(" ", fgets(STDIN));
+        $sum_cost = 0;
+        for ($j = 0; $j < $M; $j++) {
+            $sum_cost += floor($amounts[$j] * $meel_cost[$j] / 100);
+        }
+        echo $sum_cost . "\n";
+    }
+?>
