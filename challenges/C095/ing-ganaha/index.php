6e8e68baf24c58f5bc7a6f18d149d83426c3e1ed
ing-ganaha <ganaha@in-g.jp>
2025-11-09 23:51:37 +0900

    phpファイル記入

--- challenges/C095/ing-ganaha/index.php
+++ challenges/C095/ing-ganaha/index.php
@@ -1,2 +1,19 @@
 <?php
-// ここに回答を実装してください
+    list($N,$K) = explode(' ',trim(fgets(STDIN)));
+    $bass_time=[];
+    $diff_time=[];
+    $near_time=[];
+    for($i = 0;$i < $K;$i++){
+        $bass_time[$i]=(int)trim(fgets(STDIN));
+    }
+    sort($bass_time, SORT_NUMERIC);
+    for($k = 0;$k < $K;$k++){
+        $diff_time[$k]=abs($K-$bass_time[$k]);
+    }
+    $min_val=min($diff_time);
+    $near_time=array_keys($diff_time,$min_val);
+    foreach($near_time as $j){
+        echo $bass_time[$j]."\n";
+    }
+    
+?>
