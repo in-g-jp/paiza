12d779cb4dc6a1fa06504d57b19384d51aa04cba
ing-mitoma <mitoma@in-g.jp>
2026-06-02 10:47:27 +0900

    refactoring

--- challenges/C072/ing-mitoma/index.php
+++ challenges/C072/ing-mitoma/index.php
@@ -1,14 +1,14 @@
 <?php
 // ここに回答を実装してください
 [$attack, $defense, $agility] = array_map("intval", explode(" ", trim(fgets(STDIN))));
-$N = (int)trim(fgets(STDIN));
+$monsterCount = (int)trim(fgets(STDIN));
 $evolutionFound = false;
 
-for ($i = 0; $i < $N; $i++) {
+for ($i = 0; $i < $monsterCount; $i++) {
 
-  $params = explode(" ", trim(fgets(STDIN)));
-  $evolvedMonsterName = $params[0];
-  [$attackMinimum, $attackMaximum, $defenseMinimum, $defenseMaximum, $agilityMinimum, $agilityMaximum] = array_map("intval", array_slice($params, 1));
+  $evolutionCandidateData = explode(" ", trim(fgets(STDIN)));
+  $evolvedMonsterName = $evolutionCandidateData[0];
+  [$attackMinimum, $attackMaximum, $defenseMinimum, $defenseMaximum, $agilityMinimum, $agilityMaximum] = array_map("intval", array_slice($evolutionCandidateData, 1));
 
   if ($attack < $attackMinimum || $attackMaximum < $attack) {
     continue;
