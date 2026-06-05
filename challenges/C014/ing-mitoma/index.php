1a58a495f0593a0c93faaa72490dce7fae016702
ing-mitoma <mitoma@in-g.jp>
2026-06-05 13:59:39 +0900

    refactoring

--- challenges/C014/ing-mitoma/index.php
+++ challenges/C014/ing-mitoma/index.php
@@ -5,7 +5,7 @@ $diameterOfBall = 2 * $radiusOfBall;
 for ($boxNumber = 1; $boxNumber <= $totalBoxCount; $boxNumber++) {
   $boxSizes = array_map("intval", explode(" ", trim(fgets(STDIN))));
   $minimumSizeOfBox = min($boxSizes);
-  if ($diameterOfBall <= $minimumSizeOfBox) {
-    echo "$boxNumber\n";
+  if ($diameterOfBall > $minimumSizeOfBox) {
   }
+  echo "$boxNumber\n";
 }
