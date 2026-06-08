33f93e0827b165d2127e2a930a24c82e9c42bd3a
ing-mitoma <mitoma@in-g.jp>
2026-06-08 14:38:50 +0900

    refactoring2

--- challenges/B143/ing-mitoma/index.php
+++ challenges/B143/ing-mitoma/index.php
@@ -1,20 +1,18 @@
 <?php
 // ここに回答を実装してください
-[$numberOfStudents, $gameCounts] = array_map('intval', explode(" ", trim(fgets(STDIN))));
+[$numberOfStudents, $numberOfGames] = array_map('intval', explode(" ", trim(fgets(STDIN))));
 
 $queueLengths = array_fill(1, $numberOfStudents, 1);
 
-for ($i = 0; $i < $gameCounts; $i++) {
+for ($i = 0; $i < $numberOfGames; $i++) {
   [$winner, $loser] = array_map('intval', explode(" ", trim(fgets(STDIN))));
   $queueLengths[$winner] += $queueLengths[$loser];
   $queueLengths[$loser] = 0;
 }
 
 $maxLength = max($queueLengths);
-$maxLengthStudentNumbers = [];
 
 foreach ($queueLengths as $studentNumber => $queueLength) {
-  if ($queueLength === $maxLength) {
-    echo "$studentNumber\n";
-  }
+  if ($queueLength !== $maxLength) continue;
+  echo "$studentNumber\n";
 }
