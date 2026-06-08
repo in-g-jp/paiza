28dbf966108065b3e0c4535d2fe49b1ce0a5335c
ing-mitoma <mitoma@in-g.jp>
2026-06-08 14:36:14 +0900

    refactoring

--- challenges/C157/ing-mitoma/index.php
+++ challenges/C157/ing-mitoma/index.php
@@ -2,9 +2,9 @@
 // ここに回答を実装してください
 $_ = (int)trim(fgets(STDIN));
 
-$flowerCount = explode(" ", trim(fgets(STDIN)));
+$flowerCounts = explode(" ", trim(fgets(STDIN)));
 
-$uniqueFlowerCounts = array_unique($flowerCount);
+$uniqueFlowerCounts = array_unique($flowerCounts);
 $totalDifferentSeed = count($uniqueFlowerCounts);
 
 echo $totalDifferentSeed;
